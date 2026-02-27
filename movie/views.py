fb7141e6ea71ce1f89f36373087b1d5a2ac02d59
acmazoh149-crypto <acmazoh149@gmail.com>
2026-02-26 19:39:44 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html', {'name':'Greg Lim'})
+    return render(request,'home.html', {'name':'Andres Mazo'})
 
 def about(request):
     return HttpResponse('<h1> Welcome to About Page </h1>')
